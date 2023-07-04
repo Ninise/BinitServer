5bcc14f411958c82ff1c06611f1fa0c359b2af39
Nikita Nikitin <nikitin.ninise@gmail.com>
2023-07-04 06:41:48 -0400

    added sqlalchemy + postgres init

---
+++ db/base.py
@@ -0,0 +1,6 @@
+# Import all the models, so that Base has them before being
+
+from db.base_class import Base
+from models.product import Product
+from models.feedback import Feedback
+from models.suggested import Suggested
