478470bc56278fe7697a97a5ce4836fadcefec74
Nikita Nikitin <nikitin.ninise@gmail.com>
2023-07-18 20:35:09 -0400

    test port

--- main.py
+++ main.py
@@ -24,5 +24,5 @@ app.include_router(root_router)
 
 if __name__ == "__main__":
     # For debugging purposes only
-
-    unicorn.run(app, host="0.0.0.0", port=8001, log_level="debug")
+    print("DEBUG")
+    unicorn.run(app, host="0.0.0.0", port=8000, log_level="debug")
